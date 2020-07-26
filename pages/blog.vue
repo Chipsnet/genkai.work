9c03df30fd171a034921560ffc07c697ca4b51e9
Minato86 <doronomori660@outlook.jp>
2020-07-27 03:17:44 +0900

    wip

---
+++ pages/blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <pre>
+                {{ list }}
+            </pre
+        >
+    </div>
+</template>
+
+<script>
+    export default {
+        async fetch() {
+            this.list = this.$content("blog");
+        },
+    };
+</script>
