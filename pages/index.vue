bb33d4335c02817cc6371da67de7b6f73dc02100
Minato86 <doronomori660@outlook.jp>
2020-09-17 05:07:33 +0900

    top page おわり

--- pages/index.vue
+++ pages/index.vue
@@ -14,18 +14,42 @@
         </section>
         <section class="section">
             <div class="container py-6 has-text-centered">
-                <h1 class="title mt-6">{{ $t('can_title') }}</h1>
+                <h1 class="title mt-6">{{ $t("can_title") }}</h1>
+                <div class="columns my-6 is-desktop">
+                    <div class="column">
+                        <h2 class="title is-2">{{ $t("can_title1") }}</h2>
+                        <p class="subtitle is-5 mt-3">
+                            {{ $t("can_content1") }}
+                        </p>
+                    </div>
+                    <div class="column">
+                        <h2 class="title is-2">{{ $t('can_title2') }}</h2>
+                        <p class="subtitle is-5 mt-3">
+                            {{ $t("can_content2") }}
+                        </p>
+                    </div>
+                    <div class="column">
+                        <h2 class="title is-2">{{ $t('can_title3') }}</h2>
+                        <p class="subtitle is-5 mt-3">
+                            {{ $t("can_content3") }}
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
         <section class="section why">
             <div class="container py-6">
-                <h1 class="title mb-5 has-text-white">{{ $t("sec1_title") }}</h1>
+                <h1 class="title mb-5 has-text-white">
+                    {{ $t("sec1_title") }}
+                </h1>
                 <h2 class="subtitle has-text-white">
                     {{ $t("sec1_content") }}
                 </h2>
             </div>
             <div class="container py-6">
-                <h1 class="title mb-5 has-text-white">{{ $t("sec2_title") }}</h1>
+                <h1 class="title mb-5 has-text-white">
+                    {{ $t("sec2_title") }}
+                </h1>
                 <h2 class="subtitle has-text-white">
                     {{ $t("sec2_content") }}
                 </h2>
@@ -35,7 +59,9 @@
 </template>
 
 <style>
-h2 {
+h1,
+h2,
+p {
     white-space: pre-line;
 }
 
