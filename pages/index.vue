2725976eb72c2aeb2d168770431b26463d169876
Minato86 <doronomori660@outlook.jp>
2020-09-17 02:29:15 +0900

    add logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,5 @@
 <template>
     <div>
-        <div class="h-64">
-            <div class="align-middle h-full inline-block">
-                <div class="container mx-auto">
-                    <h1>aaaaaaaaaaaa</h1>
-                </div>
-            </div>
-        </div>
+
     </div>
 </template>
