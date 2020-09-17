292d2b5d094fa7382e4999ed2549863ff5ee7ea3
Minato86 <doronomori660@outlook.jp>
2020-09-17 17:01:37 +0900

    wip i18n

--- pages/index.vue
+++ pages/index.vue
@@ -23,22 +23,32 @@
                         </p>
                     </div>
                     <div class="column">
-                        <h2 class="title is-2">{{ $t('can_title2') }}</h2>
+                        <h2 class="title is-2">{{ $t("can_title2") }}</h2>
                         <p class="subtitle is-5 mt-3">
                             {{ $t("can_content2") }}
                         </p>
                     </div>
                     <div class="column">
-                        <h2 class="title is-2">{{ $t('can_title3') }}</h2>
+                        <h2 class="title is-2">{{ $t("can_title3") }}</h2>
                         <p class="subtitle is-5 mt-3">
                             {{ $t("can_content3") }}
                         </p>
                     </div>
                 </div>
+                <b-button
+                    type="is-primary"
+                    class="font"
+                    tag="router-link"
+                    to="#"
+                    >{{ $t('now_join') }}</b-button
+                >
             </div>
         </section>
         <section class="section why">
-            <div class="container py-6">
+            <div class="container pt-6 mb-5">
+                <h1 class="title is-2 has-text-white">{{ $t("why") }}</h1>
+            </div>
+            <div class="container pb-6">
                 <h1 class="title mb-5 has-text-white">
                     {{ $t("sec1_title") }}
                 </h1>
@@ -46,7 +56,7 @@
                     {{ $t("sec1_content") }}
                 </h2>
             </div>
-            <div class="container py-6">
+            <div class="container pb-6 pt-3">
                 <h1 class="title mb-5 has-text-white">
                     {{ $t("sec2_title") }}
                 </h1>
@@ -54,14 +64,20 @@
                     {{ $t("sec2_content") }}
                 </h2>
             </div>
+            <div class="container pb-6 pt-3">
+                <h1 class="title mb-5 has-text-white">
+                    {{ $t("sec3_title") }}
+                </h1>
+                <h2 class="subtitle has-text-white">
+                    {{ $t("sec3_content") }}
+                </h2>
+            </div>
         </section>
     </div>
 </template>
 
 <style>
-h1,
-h2,
-p {
+.section {
     white-space: pre-line;
 }
 
@@ -73,5 +89,11 @@ p {
         rgba(9, 9, 121, 1) 58%,
         rgba(0, 212, 255, 1) 100%
     );
+}
+
+.font {
+    font-family: a-otf-midashi-go-mb31-pr6n, sans-serif;
+    font-weight: 600;
+    font-style: normal;
 }
 </style>
